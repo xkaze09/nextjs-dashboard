4139bb9b994ba06f988368d4c37ff5384b05d11b
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-08-25 14:34:51 +0800

    Refactor dashboard page to include header and layout components; integrate RevenueChart and LatestInvoices for enhanced data visualization

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,13 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+
 export default function Page() {
-    return <p>Dashboard Page</p>;
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+            
+        </main>
+    );
 }
