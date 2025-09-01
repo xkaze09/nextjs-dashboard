58a102a4c19e384ff9cc06bd898af28a5483e62c
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-09-02 03:53:28 +0800

    Add pagination to InvoicesPage component by fetching total pages and rendering Pagination component

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -16,6 +16,7 @@ export default async function Page(props: {
       const searchParams = await props.searchParams;
       const query = searchParams?.query || '';
       const currentPage = Number(searchParams?.page) || 1;
+      const totalPages = await fetchInvoicesPages(query);
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -29,7 +30,7 @@ export default async function Page(props: {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
