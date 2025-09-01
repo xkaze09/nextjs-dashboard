244b4f86c566a4370bd8c165df683428d6cec944
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-09-02 04:43:19 +0800

    Add edit page for invoice, integrating invoice fetching and customer data retrieval for enhanced editing functionality.

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,12 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+}
