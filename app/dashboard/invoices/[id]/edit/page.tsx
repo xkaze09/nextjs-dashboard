c9868903a3503861d574cda2dc9a645e098d0375
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-09-05 18:35:47 +0800

    Add error handling for invoice retrieval in edit page, redirecting to 404 if invoice not found for improved user experience.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,14 +1,17 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
- 
-export default async function Page(props: { params: Promise<{ id: string }> }) {
-  const params = await props.params;
+import { notFound } from 'next/navigation';
+
+export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+  if (!invoice) {
+    notFound();
+  }
   return (
     <main>
       <Breadcrumbs
