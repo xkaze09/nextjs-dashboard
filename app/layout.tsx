0587da83695aed849770e1f2c6a8c5e283ad3615
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-09-07 23:28:34 +0800

    Update metadata in layout.tsx for Acme Dashboard, refining title structure and enhancing description for better SEO and clarity.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,13 @@ import { lusitana } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Acme Dashboard',
-  description: 'The official Next.js Course Dashboard, built with App Router.',
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
-
 export default function RootLayout({
   children,
 }: {
